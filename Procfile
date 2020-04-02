f1da189337a76aaa1ab5f44f93a69787e9b5b078
Emmanuel-Aggrey <aggrey.en@gmail.com>
2020-04-02 04:57:29 +0000

    inital commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn donatetome.wsgi --log-file -
---
+++ base/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.coronacheck,name='coronacheck'),
+
+    path('donate', views.index, name="index"),
+    path('charge/', views.charge, name="charge"),
+    path('success/<str:args>/', views.successMsg, name="success"),
+]
