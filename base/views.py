6170b2f8e1435e4a2874cda6c0b72336e1ac3536
Emmanuel-Aggrey <aggrey.en@gmail.com>
2020-04-26 02:23:26 +0000

    chaned api version

--- base/views.py
+++ base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.http import JsonResponse
-
+import requests
 import stripe
 
 stripe.api_key = "sk_test_cIhdY45Fv5tvwikKX6zmFwPZ00JKddro4t"
@@ -10,39 +10,40 @@ stripe.api_key = "sk_test_cIhdY45Fv5tvwikKX6zmFwPZ00JKddro4t"
 
 # Create your views here.
 
+
 def index(request):
 
-	return render(request, 'base/index.html')
+    return render(request, 'base/index.html')
 
 
 def coronacheck(request):
-    	
-    return render(request,'base/coronacheck.html')
+   
+    return render(request, 'base/coronacheck.html')
+
 
 def charge(request):
 
+    if request.method == 'POST':
+        print('Data:', request.POST)
 
-	if request.method == 'POST':
-		print('Data:', request.POST)
+        amount = int(request.POST['amount'])
 
-		amount = int(request.POST['amount'])
+        customer = stripe.Customer.create(
+            email=request.POST['email'],
+            name=request.POST['name'],
+            source=request.POST['stripeToken']
+        )
 
-		customer = stripe.Customer.create(
-			email=request.POST['email'],
-			name=request.POST['name'],
-			source=request.POST['stripeToken']
-			)
+        charge = stripe.Charge.create(
+            customer=customer,
+            amount=amount*100,
+            currency='usd',
+            description="Donation"
+        )
 
-		charge = stripe.Charge.create(
-			customer=customer,
-			amount=amount*100,
-			currency='usd',
-			description="Donation"
-			)
-
-	return redirect(reverse('success', args=[amount]))
+    return redirect(reverse('success', args=[amount]))
 
 
 def successMsg(request, args):
-	amount = args
-	return render(request, 'base/success.html', {'amount':amount})
+    amount = args
+    return render(request, 'base/success.html', {'amount': amount})
